02930a5caee09435ddeadb5b170727abb75292cf
amin110230 <aminulislamcseku@gmail.com>
2022-09-20 11:34:25 +0600

    smelly code for single responsibility principle has been added

---
+++ src/main/java/com/amin/solid/SingleResponsibilityPrinciple/smelly/Main.java
@@ -0,0 +1,27 @@
+package com.amin.solid.SingleResponsibilityPrinciple.smelly;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Item book = new Item("Java Programming", 100.0);
+        Item whiteBoard = new Item("White Board", 400.0);
+        Item markerPen = new Item("Marker Pen", 40.0);
+
+        int numberOfPlasticBags = 2;
+        int tax = 10;
+
+        var itemsPurchased = List.of(book, whiteBoard, markerPen);
+
+        Customer sadman = new Customer("Sadman Sobhan",
+                30,
+                itemsPurchased,
+                numberOfPlasticBags);
+
+        double totalPriceOfItems = sadman.calculatePrice(tax);
+
+        System.out.println("Total Prices of items purchased : " + totalPriceOfItems);
+
+    }
+}
