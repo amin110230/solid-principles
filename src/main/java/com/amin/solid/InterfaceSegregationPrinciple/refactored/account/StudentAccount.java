90584d705eb19ca8bc3a32a558809a71e6301dd0
amin110230 <aminulislamcseku@gmail.com>
2022-09-23 22:56:25 +0600

    program files have been moved in refactored folder to implement ISP

---
+++ src/main/java/com/amin/solid/InterfaceSegregationPrinciple/refactored/account/StudentAccount.java
@@ -0,0 +1,21 @@
+package com.amin.solid.InterfaceSegregationPrinciple.refactored.account;
+
+public class StudentAccount extends BaseAccount implements SavingsAccount {
+    private String institutionName;
+
+
+    public StudentAccount(int accountId, String accountName, double balance, String institutionName) {
+        super(accountId, accountName, balance);
+        this.institutionName = institutionName;
+    }
+
+    @Override
+    public void creditLocalAmount(double amountInBDT) {
+        this.balance += amountInBDT;
+    }
+
+    @Override
+    public void creditInternationalAmount(double amountInUSD) {
+        throw new UnsupportedOperationException("International amount transfer is not applicable for student account");
+    }
+}
